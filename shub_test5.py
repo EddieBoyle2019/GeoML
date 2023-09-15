3da304579ff76293ba61330abd30da81fab46ecb
EddieBoyle2019 <edwardgboyle@hotmail.com>
2023-09-15 13:48:18 +0100

    test1 of full API request and image display

--- shub_test5.py
+++ shub_test5.py
@@ -15,6 +15,8 @@ from sentinelhub import (
     bbox_to_dimensions,
 )
 
+from utils import plot_image
+
 st.title('Sentinel Hub test')
 
 config = SHConfig()
@@ -24,3 +26,50 @@ config.sh_client_secret = st.text_input("Enter your SentinelHub client secret", 
 #config.sh_client_secret = getpass.getpass("Enter your SentinelHub client secret")
 config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
 config.sh_base_url = "https://sh.dataspace.copernicus.eu"
+
+resolution = 10
+gcm_coords_wgs84 = (-3.76, 57.06, -3.72, 57.077)
+gcm_bbox = BBox(bbox=gcm_coords_wgs84, crs=CRS.WGS84)
+gcm_size = bbox_to_dimensions(gcm_bbox, resolution=resolution)
+
+evalscript_true_color = """
+    //VERSION=3
+
+    function setup() {
+        return {
+            input: [{
+                bands: ["B02", "B03", "B04"]
+            }],
+            output: {
+                bands: 3
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [sample.B04, sample.B03, sample.B02];
+    }
+"""
+
+request_true_color = SentinelHubRequest(
+    evalscript=evalscript_true_color,
+    input_data=[
+        SentinelHubRequest.input_data(
+            data_collection=DataCollection.SENTINEL2_L1C.define_from(
+                "s2l1c", service_url=config.sh_base_url
+            ),
+            time_interval=("2020-06-01", "2020-06-30"),
+            mosaicking_order=MosaickingOrder.LEAST_CC,
+        )
+    ],
+    responses=[SentinelHubRequest.output_response("default", MimeType.PNG)],
+    bbox=gcm_bbox,
+    size=gcm_size,
+    config=config,
+)
+
+true_color_imgs = request_true_color.get_data()
+
+image = true_color_imgs[0]
+
+plot_image(image, factor=3.5 / 255, clip_range=(0, 1))
