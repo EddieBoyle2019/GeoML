5d1d39dca105f9f0eecf9447a09077af10e37e1c
EddieBoyle2019 <edwardgboyle@hotmail.com>
2023-09-15 13:28:16 +0100

    test1 of st.text_input

--- shub_test5.py
+++ shub_test5.py
@@ -18,7 +18,9 @@ from sentinelhub import (
 st.title('Sentinel Hub test')
 
 config = SHConfig()
-config.sh_client_id = getpass.getpass("Enter your SentinelHub client id")
-config.sh_client_secret = getpass.getpass("Enter your SentinelHub client secret")
+config.sh_client_id = st.text_input("Enter your SentinelHub client id", type="password")
+config.sh_client_secret = st.text_input("Enter your SentinelHub client secret", type="password")
+#config.sh_client_id = getpass.getpass("Enter your SentinelHub client id")
+#config.sh_client_secret = getpass.getpass("Enter your SentinelHub client secret")
 config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
 config.sh_base_url = "https://sh.dataspace.copernicus.eu"
