bf5f7d016583471132b5d9bb60ae9a858ab99670
EddieBoyle2019 <edwardgboyle@hotmail.com>
2023-09-15 13:08:35 +0100

    sentinelhub test2

--- shub_test5.py
+++ shub_test5.py
@@ -16,3 +16,9 @@ from sentinelhub import (
 )
 
 st.title('Sentinel Hub test')
+
+config = SHConfig()
+config.sh_client_id = getpass.getpass("Enter your SentinelHub client id")
+config.sh_client_secret = getpass.getpass("Enter your SentinelHub client secret")
+config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
+config.sh_base_url = "https://sh.dataspace.copernicus.eu"
