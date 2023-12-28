76b58d7c13da85b8d190103f3bb0028986479e3f
EddieBoyle2019 <edwardgboyle@hotmail.com>
2023-12-28 09:58:00 +0000

    SCM layer attempt 5

--- utils2.py
+++ utils2.py
@@ -188,16 +188,32 @@ def request_sentinel_scm(gcm_coords_wgs84: tuple, config: SHConfig, start_date, 
         //VERSION=3
         function setup() {
             return {
-                input: ["SCL"],
+                input: ["SCL", "dataMask"],
                 output: {
-                    bands: 1,
-                    sampleType: "INT16"
+                    bands: 4,
+                    sampleType: "UINT8"
                 }
             };
         }
 
-        function evaluatePixel(sample) {
-            return [sample.SCL];
+        const classes = {
+         0: [0, 0, 0], // No Data (Missing data) - black  
+         1: [255, 0, 0], // Saturated or defective pixel - red 
+         2: [47, 47, 47], // Topographic casted shadows ("Dark features/Shadows" for data before 2022-01-25) - very dark grey
+         3: [100, 50, 0], // Cloud shadows - dark brown
+         4: [0, 160, 0], // Vegetation - green
+         5: [255, 230, 90], // Not-vegetated - dark yellow
+         6: [0, 0, 255], // Water (dark and bright) - blue
+         7: [128, 128, 128], // Unclassified - dark grey
+         8: [192, 192, 192], // Cloud medium probability - grey
+         9: [255, 255, 255], // Cloud high probability - white
+         10: [100, 200, 255], // Thin cirrus - very bright blue
+         11: [255, 150, 255], // Snow or ice - very bright pink
+        }
+
+        function evaluatePixel(samples) {
+            let imgVals = classes[samples.SCL] || [0, 0, 0];
+            return imgVals.concat(samples.dataMask * 255);
         }
     """
 
