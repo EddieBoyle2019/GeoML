ab3995fd9247fc4a0c6fd142ce3fdb4126df29e8
EddieBoyle2019 <edwardgboyle@hotmail.com>
2024-01-07 11:18:26 +0000

    Experiment with rgb values 24

--- utils2.py
+++ utils2.py
@@ -219,8 +219,8 @@ def request_sentinel_scm(gcm_coords_wgs84: tuple, config: SHConfig, start_date, 
                 // Snow or ice (very bright pink)
                 //case 11: return RGBToColor (255, 150, 255, samples.dataMask);
                 //case 11: return RGBToColor (255, 100, 255, samples.dataMask);
-                //case 11: return [1, 0, 1, samples.dataMask];
-                case 11: return [255, 150, 255, samples.dataMask];
+                case 11: return [1, 0, 1, samples.dataMask];
+                //case 11: return [255, 150, 255, samples.dataMask];
                 //default : return RGBToColor (0, 0, 0, samples.dataMask);  
             }
         }
