4308ff035764921d98306327b656868164d2e04b
EddieBoyle2019 <edwardgboyle@hotmail.com>
2024-03-24 07:50:35 +0000

    new prototype; new utils, added lat/long to marker tooltip edit3

--- utils3.py
+++ utils3.py
@@ -33,7 +33,7 @@ def plot_map_with_image(
 
     folium.Marker(
         location=centre[::-1],
-        tooltip=f"{name} snow patch; Lat/Long: {centre[1].2f, centre[0].2f}",
+        tooltip=f"{name} snow patch; Lat/Long: {centre[1]:.2f}, {centre[0]:.2f}",
         icon=folium.Icon(),
     ).add_to(m)
 
